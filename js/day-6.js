339b93613a74a6c6ee18187117629f41b339a613
Olalekan <lekan.olapeju@yahoo.com>
2020-03-10 09:58:35 -0600

    Added more scope examples

--- js/day-6.js
+++ js/day-6.js
@@ -12,7 +12,23 @@
      console.log( myVariable ); //Can we myVariable in the function!?  Yes! We can see GLOBALLY scoped variables inside of a function!
 
      var myFunctionScopeVariable = 64; // Declared in a local (function) scope.
+
+     console.log( myFunctionScopeVariable ); // We can see it in the function :D
+
+     function myNestedFunction () { // (Yes.)
+         var myNestedFunctionVariable = 'I\'m very nested!';
+         console.log( myNestedFunctionVariable ); // Local access works!
+
+         console.log( 'Accessing function scope variable in nested function: '+ myFunctionScopeVariable ); //We can access variables from a PARENT function. But not the revesrse.
+     }
+     myNestedFunction();
+
+     console.log( myVariable ); //Global variables / functions are accessible even all the way in here! NICE!
+
+     //console.log( myNestedFunctionVariable ); // The variable is scoped to its origin function, not here...
  }
 
  myFunction();
- console.log( myFunctionScopeVariable ); // Can we access it outside of the function!?
+ //console.log( myFunctionScopeVariable ); // Can we access it outside of the function!?
+
+ //myNestedFunction(); // Can we run the nested function!? Nope! It is not available globallly if it was defined in a localised scope! 
